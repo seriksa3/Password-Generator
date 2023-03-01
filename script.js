fdf9ef17341075ee1d353ca5e94f23322e643dac
seriksa3 <kellyseriksa7@gmail.com>
2023-03-01 10:57:33 +0000

    Asiignment submit

--- script.js
+++ script.js
@@ -87,34 +87,87 @@ var specialCharacters = [
     'Y',
     'Z'
   ];
-  
-  // Function to prompt user for password options
-  function getPasswordOptions() {
-    for(let a = 0; a>specialCharacters.length;a++){
-        var passwordCharacter = specialCharacters[a];
-    }
-  }
-  
-  // Function for getting a random element from an array
-  function getRandom(arr) {
-  
-  }
-  
-  // Function to generate password with user input
+  // 
   function generatePassword() {
 
+    let passwordLength;
+    while (true) {
+      passwordLength = prompt(
+        "\nPlease enter the number of characters you'd like in your password? \n(Must be between 8-100 characters)\n"
+      );
+  
+      if (passwordLength >= 8 && passwordLength <= 100) {
+        alert("\nGreat Choice!" + "\nYour password will have " + passwordLength + " characters");
+        break;
+      } else {
+        alert("Opps! The value you entered is incorrect" + "Click 'OK' to try again");
+      }
+    }
+  
+    let userInputNum = parseInt(passwordLength);
+    let selectChar = [];
+  
+    while (true) {
+      let includeLowerChar = confirm(
+        "\nWould you like your password to include Lowercase? OK for 'YES' Cancel for 'NO'"
+      );
+      let includeUpperChar = confirm(
+        "\nWould you like your password to include Uppercase? OK for 'YES' Cancel for 'NO'"
+      );
+      let includeNumbers = confirm(
+        "\nWould you like your password to include Numbers? OK for 'YES' Cancel for 'NO'"
+      );
+      let includeSpecialChar = confirm(
+        "\nFinally, Would you like your password to include Special Characters? OK for 'YES'Cancel for 'NO'"
+      );
+  
+      if (includeLowerChar) {
+        selectChar = selectChar.concat(lowerCasedCharacters);
+      }
+      if (includeUpperChar) {
+        selectChar = selectChar.concat(upperCasedCharacters);
+      }
+      if (includeNumbers) {
+        selectChar = selectChar.concat(numericCharacters);
+      }
+      if (includeSpecialChar) {
+        selectChar = selectChar.concat(specialCharacters);
+      }
+  
+      if (selectChar.length > 0) {
+        break;
+      } else {
+        alert("\nYou must select at least one Character type" + "Try again");
+      }
+    }
+  
+    let result = [];
+    for (let i = 0; i < selectChar.length; i++) {
+      result.push(randomChar(selectChar));
+    }
+
+    const password = result.slice(0, userInputNum).join("");
+  
+    return password;
   }
   
-  // Get references to the #generate element
-  var generateBtn = document.querySelector('#generate');
+  // Function to generate a random number
+  function randomChar(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
   
-  // Write password to the #password input
+  //Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  
+  //Write password to the #password input 
   function writePassword() {
     var password = generatePassword();
-    var passwordText = document.querySelector('#password');
+    var passwordText = document.querySelector("#password");
   
     passwordText.value = password;
   }
   
-  // Add event listener to generate button
-  generateBtn.addEventListener('click', writePassword);
+  //Add event listener to generate button 
+  generateBtn.addEventListener("click", writePassword);
+
+  
